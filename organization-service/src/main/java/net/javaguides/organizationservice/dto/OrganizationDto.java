b181668578dea0ae8b15eef0f9a3f0693a2ce6e7
thsunith <thsunith@INLEN8520087786.corp.capgemini.com>
2023-11-28 19:44:11 +0530

    test commit

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/dto/OrganizationDto.java
@@ -0,0 +1,63 @@
+package net.javaguides.organizationservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrganizationDto {
+    private Long id;
+    private String organizationName;
+    private String organizationDescription;
+    private String organizationCode;
+    private LocalDateTime createdDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOrganizationName() {
+		return organizationName;
+	}
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+	public String getOrganizationDescription() {
+		return organizationDescription;
+	}
+	public void setOrganizationDescription(String organizationDescription) {
+		this.organizationDescription = organizationDescription;
+	}
+	public String getOrganizationCode() {
+		return organizationCode;
+	}
+	public void setOrganizationCode(String organizationCode) {
+		this.organizationCode = organizationCode;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public OrganizationDto(Long id, String organizationName, String organizationDescription, String organizationCode,
+			LocalDateTime createdDate) {
+		super();
+		this.id = id;
+		this.organizationName = organizationName;
+		this.organizationDescription = organizationDescription;
+		this.organizationCode = organizationCode;
+		this.createdDate = createdDate;
+	}
+	public OrganizationDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
+}
---
+++ organization-service/src/main/java/net/javaguides/organizationservice/entity/Organization.java
@@ -0,0 +1,72 @@
+package net.javaguides.organizationservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "organizations")
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String organizationName;
+    private String organizationDescription;
+    @Column(nullable = false, unique = true)
+    private String organizationCode;
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOrganizationName() {
+		return organizationName;
+	}
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+	public String getOrganizationDescription() {
+		return organizationDescription;
+	}
+	public void setOrganizationDescription(String organizationDescription) {
+		this.organizationDescription = organizationDescription;
+	}
+	public String getOrganizationCode() {
+		return organizationCode;
+	}
+	public void setOrganizationCode(String organizationCode) {
+		this.organizationCode = organizationCode;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Organization(Long id, String organizationName, String organizationDescription, String organizationCode,
+			LocalDateTime createdDate) {
+		super();
+		this.id = id;
+		this.organizationName = organizationName;
+		this.organizationDescription = organizationDescription;
+		this.organizationCode = organizationCode;
+		this.createdDate = createdDate;
+	}
+	public Organization() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+}
